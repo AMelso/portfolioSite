f591467e15848c453c6d3fbf1e7861da92ed7b79
Anthony M <anthony.melsoner@grainger.com>
2023-04-13 22:07:25 -0400

    featured / all post coming from directory

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,28 @@
 import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-post";
-
 import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../lib/post-util";
 
-const DUMMY_POSTS = [
-    {
-        slug: "1st-App",
-        title: "1st App",
-        image: 'getting-started-nextjs.png',
-        date: '2023-02-02',
-        excerpt: 'Dummy Post',
-    },
-    {
-        slug: "2nd-App",
-        title: "2nd App",
-        image: 'getting-started-nextjs.png',
-        date: '2023-02-02',
-        excerpt: 'Dummy Post',
-    },
-    {
-        slug: "3rd-App",
-        title: "3rd App",
-        image: 'getting-started-nextjs.png',
-        date: '2023-02-02',
-        excerpt: 'Dummy Post',
-    },
-    {
-        slug: "4th-App",
-        title: "4th App",
-        image: 'getting-started-nextjs.png',
-        date: '2023-02-02',
-        excerpt: 'Dummy Post',
-    },
-]
 
-function HomePage() {
+
+function HomePage(props) {
     return (
         <Fragment>
             <Hero />
-            <FeaturedPosts posts={DUMMY_POSTS} />
+            <FeaturedPosts posts={props.posts} />
         </Fragment>
     )
 }
 
+export function getStaticProps() {
+    const featuredPosts = getFeaturedPosts();
+
+    return {
+        props: {
+            posts: featuredPosts
+        }
+    }
+}
 export default HomePage;
 
 // 1) Hero => Present Self
---
+++ posts/1st-App.md
@@ -0,0 +1,11 @@
+---
+title: 'First post'
+date: '2022-10-22'
+image: getting-started-nextjs.png
+except: this is the content of the post
+isFeatured: true
+---
+
+# This is a title
+
+This is some regular text with [link](https://google.com)
